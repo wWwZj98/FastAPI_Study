f7bc6ec4f908f12ab925d5769fff50ba8decc51f
xld <celine.xiu@outlook.com>
2026-06-16 00:30:25 +0800

    FastAPI first program

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from fastapi import FastAPI
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+# 创建FastAPI实例
+app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"message": f"Hello {name}"}
